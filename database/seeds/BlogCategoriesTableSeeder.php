fbf2aaab8f21150fab7d1afe2b0c908cd798dbe3
Роман Аверкин <romaaverkin@gmail.com>
2020-04-30 15:11:43 +0700

    Initial commit

---
+++ database/seeds/BlogCategoriesTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BlogCategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [];
+
+        $cName = 'Без категории';
+        $categories[] = [
+            'title' => $cName,
+            'slug' => Str::slug($cName),
+            'parent_id' => 0
+        ];
+
+        for ($i = 0; $i <= 10; $i++) {
+            $cName = "Категоря #" . $i;
+            $parent_id = ($i > 4) ? rand(1, 4) : 1;
+
+            $categories[] = [
+                'title' => $cName,
+                'slug' => Str::slug($cName),
+                'parent_id' => $parent_id
+            ];
+        }
+
+        \DB::table('blog_categories')->insert($categories);
+    }
+}
